2f2cc20e7bc95b0e78f787225b8eb74537f6ded6
Stefan K Loesch <sklgit@oditorium.com>
2014-09-13 11:40:30 +0100

    --  content: guinea pigs

--- _guineapigs.php
+++ _guineapigs.php
@@ -9,8 +9,25 @@ GUINEA PIGS
 
 global $pg_main;
 
-$pg_main = <<<END
+ob_start(); ?>
 <h2>Guinea Pigs</h2>
-<em>to come</em>
-END;
+<p><em>-to come-</em></p>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus et lacus in arcu euismod mollis mollis in enim. Maecenas commodo feugiat pretium. Vestibulum aliquam faucibus mauris non tincidunt. Curabitur varius scelerisque arcu, et vehicula felis convallis nec. Maecenas hendrerit feugiat aliquet. Donec sodales, ipsum vel vulputate finibus, augue tortor accumsan dui, a pretium ligula diam quis nibh. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vitae gravida erat. Sed nec elementum diam, ac commodo massa. Nullam ut purus et eros ullamcorper dapibus in consectetur velit. Integer sed ante sed lorem egestas commodo at ac massa. Quisque eget nunc risus. Morbi dignissim sem et arcu faucibus, nec consequat libero pharetra. Suspendisse semper mollis lorem, at maximus massa laoreet eu.</p>
+
+<h3>Feeding</h3>
+
+<p>Curabitur quis risus viverra, consequat tortor et, rhoncus risus. Mauris mauris sem, sollicitudin quis egestas at, malesuada auctor nunc. Donec euismod orci in finibus lacinia. Phasellus feugiat vehicula ante ut accumsan. Praesent ut pellentesque leo. Morbi dignissim, ipsum a lacinia dapibus, lacus enim posuere ligula, ac euismod ex nisi in sapien. Nunc hendrerit magna odio, eget fermentum nunc fermentum a. Maecenas lobortis ex tincidunt tellus pulvinar accumsan. Etiam lorem massa, congue et sodales vel, gravida quis eros. Donec quis nibh ante.</p>
+
+<h3>Taking care of them</h3>
+
+<p>Mauris nec dolor commodo purus condimentum fermentum. Maecenas posuere ipsum lacus, ac interdum ligula fringilla scelerisque. Nam in ex augue. Nullam magna est, elementum nec euismod sed, finibus at velit. Praesent ut magna sed massa porta molestie nec eu augue. Proin laoreet nibh orci, quis porttitor risus imperdiet non. Sed a nunc mauris. Ut molestie a tortor sit amet ultricies. Duis tempus arcu turpis, id hendrerit justo pretium in. Nam consectetur, leo ut ultricies consectetur, orci magna tincidunt metus, in laoreet urna purus in metus. Cras a sollicitudin elit. Curabitur ut ullamcorper elit.</p>
+
+<h3>Vaccinations</h3>
+
+<p>Vestibulum viverra consectetur nunc et viverra. Quisque mattis neque nec nulla hendrerit facilisis. Vestibulum ut imperdiet mauris. Duis semper ipsum ut tincidunt placerat. Quisque ultrices ultricies cursus. Nulla iaculis tincidunt ipsum molestie vehicula. Sed imperdiet euismod dui nec hendrerit. Fusce sit amet dui venenatis, porta est nec, rutrum urna. Sed mattis neque id pellentesque accumsan. Phasellus id consequat augue. Nulla accumsan magna et eros rutrum, in aliquet urna congue. Morbi vel ipsum vitae risus tempus pretium vel at neque. Cras eget tortor lacus. Etiam interdum lorem blandit, auctor diam sed, tempor enim. Sed non tortor molestie, bibendum ex eget, congue ligula.</p>
+
+<?php
+global $pg_main;
+$pg_main = ob_get_clean();
 
