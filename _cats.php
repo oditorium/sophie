9416d54fa655ab81a5824990c147222a2af581a4
Stefan K Loesch <sklgit@oditorium.com>
2014-09-13 11:30:54 +0100

    --  content: cats

--- _cats.php
+++ _cats.php
@@ -6,11 +6,44 @@ CATS
 
 */
 
-
-global $pg_main;
-
-$pg_main = <<<END
+ob_start(); ?>
 <h2>Cats</h2>
-blabla
-END;
+
+<p>Cats mostly live in the house or apartment of the family, but they can also live outside, for example on a farm. They do not a lot of space, so they are not usually held in cages, unless they are travelling. Most cats enjoy going outside <img src='img/6x6/cat-snow.jpg' class='onpage left'/>
+	from time to time, even though cats living in the big cities might stay in the house all the time. When cats go outside, they often go hunting, for example birds or mice. Well-fed cats usually do not eat what they have hunted, but they bring it as a present for their owner.</p>
+
+
+
+<p>If they are allowed to and (and sometimes even if they aren't) they sleep in soft places, for example on the sofa, on pillows, or - especially if it is cold - in their owner's bed. Some cats even manage to open doors to get to the sleeping place of their choice. Sometimes it is also possible to 
+convince cats to sleep in their basket, but this is rather rare 
+<img src='img/6x6/cat-kitten.jpg' class='onpage right'/>
+because cats don't like to be told what to do.</p>
+
+<h3>Feeding</h3>
+
+<p>Cats can eat very different things, depending on what they have been fed when they were kittens. For example, I had a cat who would only eat one particular kind of cat-food and who would refuse everything else - fish, meat, other cat
+ food, it would not matter. Some cats also drink milk even when they are adult, but some only drink it when they are kittens.</p>
+
+<p>All cats drink water - they actually slurp it up with their tongues. 
+<img src='img/6x6/cat-silver.jpg' class='onpage left'/>
+Some cats are very particular about the water they are drinking. For example, my cat would prefer to drink the water directly from the tap, and would only from its bowl when the water was fresh. He would also drink from clean paddles outside, but not from stale water in a bowl.</p>
+
+<p>Cats also eat grass when they are outside, which is good their digestions. Cats that go outside will usually eat grass where they find it, but if they live inside all the time their owners might have to find a way of feeding their 
+cat some fresh grass.</p>
+
+<h3>Taking care of them</h3>
+
+<p>Taking care of cats is easy - you only need to feed them, and let them in and out whenever they choose to, everything else they do themselves. 
+<img src='img/6x6/cat-brown.jpg' class='onpage right'/>
+Transporting them - for example to the vet - can be difficult: my cat managed to open the transport cage and jumped out, fortunately before we even started driving, otherwise this could have been very dangerous.</p>
+
+<p>When cats can go out often they usually go to the toilet outside. If they mostly live inside then they can be easily trained to go to their own 'cat's loo' that then needs to be cleaned and refilled with cat litter about once a day. There is no need to take cats out for a walk as you would do with dogs, because they go their own ways.</p>
+
+<h3>Vaccinations</h3>
+
+<p>Cats who go outside tend to roam quite far away from their house, and usually they meet animals that live in the wild  - sometimes they can be gone for days before they come back. In areas with rabies it is very important that cats are vaccinated every year so that they don't get infected and in turn can infect their owners, which is very dangerous.</p>
+
+<?php
+global $pg_main;
+$pg_main = ob_get_clean();
 
